e1801ff2d9fb5dcf8f1f06858167dc84731687e9
Daniel Salgado <danielsalgado@unitec.edu>
2016-07-29 16:38:34 -0600

    Todo Funciona

--- main.cpp
+++ main.cpp
@@ -3,9 +3,10 @@
 using std::cout; 
 using std::cin; 
 using std::endl;
-
+using namespace std;
 unsigned long int factorial(unsigned long int);
 int identificar(int);
+int sumaDivisores(int);
 int main(int argc,char* argv[]){
 	int op=0;
 	while(op != 3){
@@ -28,8 +29,20 @@ int main(int argc,char* argv[]){
 		}
 		if(op == 2){
 			int numero;
-			cout << "Ingrese Un Numero: " << endl;
+			int numero2;
+			int suma1;
+			cout << "Ingrese Primer Numero: " << endl;
 			cin >> numero;
+			cout << "Ingrese Segundo Numero: " << endl;
+			cin >> numero2;
+			suma1 = sumaDivisores(numero);
+			if(sumaDivisores(suma1) == numero){
+				cout << "Numbers " << numero << " and " << numero2 << " are friends" << endl;	
+			}
+			else{
+				cout << "Numbers aren't friends" << endl;
+			}
+			 
 		}
 
 	}
@@ -53,4 +66,13 @@ int identificar(int numero){
                       	return suma;
 			
 
+}
+int sumaDivisores(int numero){
+		int suma = 1;
+		for(int i=2; i<=(numero/2); i++){
+			if(numero%i == 0){
+				suma +=i;
+			}
+		}
+		return suma;
 }
