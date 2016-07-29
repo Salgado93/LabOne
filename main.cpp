887d6344193df53477d67946b8dd992891affe5d
Daniel Salgado <danielsalgado@unitec.edu>
2016-07-29 16:03:54 -0600

    Comentarios Eliminados

--- main.cpp
+++ main.cpp
@@ -16,43 +16,26 @@ int main(int argc,char* argv[]){
 		cin >> op;
 		cout << endl;
 		if (op == 1){
-			int x=1;
-			//for(int i=1; i<=200; i++){
-				while(x<=1000000){
-					if (x == identificar(x)){
-						cout << "El numero: " << x << " Cumple" << endl; 
-						x++;
-					}else{
-						x++;
-					}
-					
+			int numero=1;
+			while(numero<=1000000){
+				if (numero == identificar(numero)){
+					cout << "El numero: " << numero << " Cumple" << endl; 
+					numero++;
+				}else{
+					numero++;
 				}
-				//cout << identificar(40585) << endl;
-			//}
-			/*int numero=0;
-			int numero2=0;
-			int suma=0;
-			int resto=0;
-			int suma2;
-			cout << "Ingrese Un Numero: ";
-			cin >> numero;
-			numero2=numero; 
-			while (numero>0){
-				resto=numero%10;
-				numero=numero/10;
-				suma+=factorial(resto);
-				suma2=suma;
-				cout << resto << "!" << endl;
 			}
-			cout << endl;
-			if(suma == numero2){
-				cout << "La suma de la factorial es igual al numero ingresado." << endl;
-				cout << "Suma: " << suma2 << endl;
-			}*/				
 		}
-	} 	
+		if(op == 2){
+			int numero;
+			cout << "Ingrese Un Numero: " << endl;
+			cin >> numero;
+		}
+
+	}
 	return 0;
 }
+
 unsigned long int factorial(unsigned long int n){
 	if (n == 0 || n== 1)
 		return 1;
